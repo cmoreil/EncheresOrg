aa4fb8071eacd2378a93bfc43859a4c52ef52fe5
AD\cmoreil2022 <caroline.moreil2022@campus-eni.fr>
2022-05-19 11:25:53 +0200

    petits changements à la marge

--- src/main/java/fr/eni/ecole/Encheres/bll/bo/SomeDailyJob.java
+++ src/main/java/fr/eni/ecole/Encheres/bll/bo/SomeDailyJob.java
@@ -1,13 +1,16 @@
 package fr.eni.ecole.Encheres.bll.bo;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import fr.eni.ecole.Encheres.bll.managers.ArticleManager;
 import fr.eni.ecole.Encheres.bll.managers.ManagerFactory;
 import fr.eni.ecole.Encheres.modeles.bll.bo.Article;
-import fr.eni.ecole.Encheres.modeles.bll.bo.SellStatus;
 
 public class SomeDailyJob implements Runnable {
 
@@ -16,13 +19,25 @@ public class SomeDailyJob implements Runnable {
 	
 	@Override
 	public void run() {
-		System.out.println("je passe dans runnable");
+		LocalDateTime dateHeureJour = LocalDateTime.now();
 		LocalDate dateDuJour = LocalDate.now();
+		Path path;
+		
 		articles = articleManager.findAll();
 		for(int i = 0; i < articles.size(); i++) {
 			if(articles.get(i).getAuctionStartDate().isBefore(dateDuJour)){
 				articleManager.changeStatus(articles.get(i));
 			}
 		}
-	}
+		
+		path = Paths.get("D:/fichier"+dateDuJour+".txt");
+			try {
+				String str = "Traitement fait le " + dateHeureJour;
+				byte[] bs = str.getBytes();
+				Path writtenFilePath = Files.write(path, bs);
+				new String(Files.readAllBytes(writtenFilePath));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 }
